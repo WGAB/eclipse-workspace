d3496855b1726f63900e8ee8c8e852be48dea290
Winston Bezerra <maxieficiencia@gmail.com>
2021-10-04 15:54:53 -0300

    Atualização das variáveis

--- eclipse-workspace/primeiro/programa/baseStart.java
+++ eclipse-workspace/primeiro/programa/baseStart.java
@@ -5,7 +5,11 @@ public class baseStart {
 	public static void main(String[] args) {
 		System.out.println("Olá de novo Mundo, fazendo novo commit!!");
 		System.out.println("Qualquer coisa !");
+		
+		private String nome = "Progamador BR";
+		private int idade = 40;
 
+		System.out.println(nome + " tem " + idade + " anos ");
 	}
 
 }
