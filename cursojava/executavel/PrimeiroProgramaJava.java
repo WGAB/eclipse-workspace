e620c7c6670ec1243e3427dd7d6f2c75a6e7e253
Winston <maxieficiencia@gmail.com>
2022-03-05 10:12:37 -0300

    This is how that works!
    Change-Id: I7d9ea1f7489a301e1f4dec29361593fa7056118f

--- cursojava/executavel/PrimeiroProgramaJava.java
+++ cursojava/executavel/PrimeiroProgramaJava.java
@@ -2,6 +2,6 @@ package cursojava.executavel;
 
 public class PrimeiroProgramaJava {
 	public static void main(String[] args) {
-		System.out.println("It Works fine!");
+		System.out.println("This is how Work a new Push!");
 	}
 }
