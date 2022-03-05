1bc9dd7a00b8b31d71d958112a76058841295571
Winston <maxieficiencia@gmail.com>
2022-03-05 13:06:06 -0300

    New commit

--- cursojava/executavel/PrimeiroProgramaJava.java
+++ cursojava/executavel/PrimeiroProgramaJava.java
@@ -3,5 +3,6 @@ package cursojava.executavel;
 public class PrimeiroProgramaJava {
 	public static void main(String[] args) {
 		System.out.println("This is how Work a new Push!");
+		System.out.println("Keep Calm and fill the data!");
 	}
 }
