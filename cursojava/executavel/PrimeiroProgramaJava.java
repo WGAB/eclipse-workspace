09063f5ddc4976482b116efe363d9762fc2eb078
Winston <maxieficiencia@gmail.com>
2022-03-13 09:07:28 -0300

    refactoring the code

--- cursojava/executavel/PrimeiroProgramaJava.java
+++
@@ -1,8 +0,0 @@
-package cursojava.executavel;
-
-public class PrimeiroProgramaJava {
-	public static void main(String[] args) {
-		System.out.println("This is how Work a new Push!");
-		System.out.println("Keep Calm and fill the data!");
-	}
-}
---
+++ src/cursojava/executavel/PrimeiroProgramaJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+import java.lang.*;
+
+public class PrimeiroProgramaJava {
+	public static void main(String[] args) {
+
+System.out.println("This is how Work a new Push!");
+System.out.println("Keep Calm and fill the data!");
+System.out.println("It Works fine!");
+System.out.println("This is how that works!!");
+
+	}
+}
