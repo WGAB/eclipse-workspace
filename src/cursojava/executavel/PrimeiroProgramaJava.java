c95e6d16a6b7be2d5022550143ac76b1254e5883
Winston <maxieficiencia@gmail.com>
2022-03-14 09:13:02 -0300

    Update!

--- src/cursojava/executavel/PrimeiroProgramaJava.java
+++ src/cursojava/executavel/PrimeiroProgramaJava.java
@@ -4,10 +4,25 @@ import java.lang.*;
 public class PrimeiroProgramaJava {
 	public static void main(String[] args) {
 
-System.out.println("This is how Work a new Push!");
-System.out.println("Keep Calm and fill the data!");
-System.out.println("It Works fine!");
-System.out.println("This is how that works!!");
+	double[] valores = new double[5];
+		
+	valores [0] = 0.1;
+	valores [1] = 0.5;
+	valores [2] = 0.7;
+	valores [3] = 0.9;
+	valores [4] = 2.1;
+		
+	// Como mostrar todos os elementos do Array?
+for (double elemento: valores) {
+	System.out.println(elemento);
+}
+
+	// Como somar todos os elementos?
+double total = 0;
+for (int i = 0; i < valores.length; i++) {
+	total += valores[i];
+}
+System.out.println("O total é " + total);
 
 	}
 }
